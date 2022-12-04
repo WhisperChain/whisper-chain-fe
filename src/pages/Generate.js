240511484042f6b189146eaaa509fe159d84f3df
mohitcharkha <mohit@plgworks.com>
2022-12-04 06:56:25 +0530

    Added more filter options

--- src/pages/Generate.js
+++ src/pages/Generate.js
@@ -277,7 +277,47 @@ const filterOptions = [
   {
     label: "CINEMATIC",
     value:
-      "sharp focus, emitting diodes, smoke, artillery, sparks, racks, system unit, motherboard, by pascal blanche rutkowski repin artstation hyperrealism painting concept art of detailed character design matte painting, 4 k resolution blade runner",
+      "perfect composition, beautiful detailed intricate insanely detailed octane render trending on artstation, 8 k artistic photography, photorealistic concept art, soft natural volumetric cinematic perfect light, chiaroscuro, award - winning photograph, masterpiece, oil on canvas, raphael, caravaggio, greg rutkowski, beeple, beksinski, giger",
+  },
+  {
+    label: "DELICATE DETAIL",
+    value:
+      "trending on artstation, sharp focus, studio photo, intricate details, highly detailed, by greg rutkowski",
+  },
+  {
+    label: "SATURATED SPACE",
+    value:
+      "outer space, vanishing point, super highway, high speed, digital render, digital painting, beeple, noah bradley, cyril roland, ross tran, trending on artstation",
+  },
+  {
+    label: "NEON MECHA",
+    value:
+      "neon ambiance, abstract black oil, gear mecha, detailed acrylic, grunge, intricate complexity, rendered in unreal engine, photorealistic",
+  },
+  {
+    label: "ETHEREAL LOW POLY",
+    value:
+      "low poly, isometric art, 3d art, high detail, artstation, concept art, behance, ray tracing, smooth, sharp focus, ethereal lighting",
+  },
+  {
+    label: "WARM BOX",
+    value:
+      "golden ratio, fake detail, trending pixiv fanbox, acrylic palette knife, style of makoto shinkai studio ghibli genshin impact james gilleard greg rutkowski chiho aoshima",
+  },
+  {
+    label: "WASTELAND",
+    value:
+      "isometric, digital art, smog, pollution, toxic waste, chimneys and railroads, 3 d render, octane render, volumetrics, by greg rutkowski",
+  },
+  {
+    label: "SPIELBERG",
+    value:
+      "cinematic, 4k, epic Steven Spielberg movie still, sharp focus, emitting diodes, smoke, artillery, sparks, racks, system unit, motherboard, by pascal blanche rutkowski repin artstation hyperrealism painting concept art of detailed character design matte painting, 4 k resolution blade runner",
+  },
+  {
+    label: "FLAT PALLETTE",
+    value:
+      "acrylic painting, trending on pixiv fanbox, palette knife and brush strokes, style of makoto shinkai jamie wyeth james gilleard edward hopper greg rutkowski studio ghibli genshin impact",
   },
   {
     label: "LUSH ILLUMINATION",
@@ -317,8 +357,9 @@ function Generate() {
       const pubId = (await getPublication("0x59cf", 1)).data.publications
         .items[0].id;
 
-      const comment = await (await getCommentFeed(pubId, 1)).data.publications
-        .items[0];
+      const comment = await (
+        await getCommentFeed(pubId, 1)
+      ).data.publications.items[0];
       console.log({ pubId });
       setPubsId(pubId);
       setPreviousImageUrl(
