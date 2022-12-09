10d17a621c088a7bb800fce5e2432bea7652683b
sachingadhari <37650415+GadhariSachin@users.noreply.github.com>
2022-12-09 18:06:48 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+// eslint-disable-next-line
+import 'symbol-observable';
+
 import "../styles/globals.css";
 
 // store imports
 import { Provider } from "react-redux";
 import { reduxWrapper } from "../store/store";
-
 
 function MyApp({ Component, pageProps, ...rest }) {
   const { store } = reduxWrapper.useWrappedStore(rest);
