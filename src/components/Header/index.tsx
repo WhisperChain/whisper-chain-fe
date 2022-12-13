99a5d3fd46d9254e825b6725d0052c4302bd4a4e
Hardik Shah <hardikshah_2000@yahoo.com>
2022-12-13 23:10:21 +0530

    added onclick modal for sign in btn

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,31 @@ import React from "react";
 import Logo from "../../assets/Logo";
 import CustomConnectButton from "../ConnectButton";
 import styles from "./Header.module.css";
+import Modal from "react-modal";
 
 const Header = () => {
+
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const customStyles = {
+  content: {
+          background: '#FFFFFF',
+          height:"fit-content",
+          width: 314,
+          margin: 'auto',
+          backdropFilter: "blur(60px)",
+          borderRadius: "16px",
+  },
+  overlay: {
+     background: "rgba(0, 0, 0, 0.6)"
+   }
+};
+
   return (
     <div className={`px-[50px] ${styles.HeadComponent}`} id="Header">
       <Logo />
@@ -11,11 +34,23 @@ const Header = () => {
         <button className={` w-[92px] h-[36px] bg-[#FFFFFF] rounded-[40px] ${styles.DropdownBtn}`}>
             Twitter
         </button>
-        <button className={` w-[92px] h-[36px] bg-[#FFFFFF] rounded-[40px] ${styles.SignInbtn}`}>
+        <button onClick={handleOpen} className={` w-[92px] h-[36px] bg-[#FFFFFF] rounded-[40px] ${styles.SignInbtn}`}>
             Sign In
         </button>
         <CustomConnectButton />
       </div>
+      <Modal
+        onRequestClose={handleClose}
+        isOpen={open}
+        style={customStyles}
+      >
+        <div className={`flex flex-col justify-center items-center bg-[#FFFFFF] rounded-[16px] backdrop-blur-3xl gap-[16px] p-[19px] ${styles.ModalContainer}`}>
+            <div className="flex ">Sign In</div>
+            <button className={`flex justify-center box-border items-center w-[234px] h-[40px] backdrop-blur rounded-[4px] ${styles.TwitterContainer}`}>Sign in with Twitter</button>
+            <button className={`flex justify-center box-border items-center w-[234px] h-[40px] backdrop-blur rounded-[4px] ${styles.LensContainer}`}>Sign in with Lens</button>
+            <button className={`flex justify-center box-border items-center w-[234px] h-[40px] backdrop-blur rounded-[4px] ${styles.FarcasterContainer}`}>Sign in with farcaster</button>
+        </div>
+      </Modal>
     </div>
   );
 };
