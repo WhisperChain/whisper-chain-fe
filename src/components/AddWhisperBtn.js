b38625d8d603ad6f72eefdf8ad97cde9a0f8df9a
Hardik Shah <hardikshah_2000@yahoo.com>
2022-12-16 09:49:21 +0530

    removed sc from add whisper btn

--- src/components/AddWhisperBtn.js
+++ src/components/AddWhisperBtn.js
@@ -1,46 +1,23 @@
 import React from "react";
-import styled from "styled-components";
 import { useBottomTab } from "../context/BottomTabContext";
 import { usePublicationContext } from "../context/PublicationContext";
 import { TabItems } from "./Main/TabItems";
+import styles from "./AddWhisperBtn.module.css";
 
-const ButtonBg = styled.div`
-  width: 200px;
-  height: 48px;
-  background: radial-gradient(107.14% 107.14% at 50% 80.95%, #6F1AFF 0%, #C6A4FF 100%);
-  box-shadow: 0px 8px 32px rgba(111, 26, 255, 0.32), inset 0px -4px 8px rgba(185, 143, 255, 0.6), inset 0px -8px 16px #4700C0;
-  border-radius: 40px;
-  margin-top: 50px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index:10;
-  cursor:pointer;
-`;
-
-const ButtonText = styled.div`
-  font-style: normal;
-  font-weight: 800;
-  font-size: 16px;
-  line-height: 100%;
-  text-align: center;
-  letter-spacing: -0.03em;
-  color: #FFFFFF;
-`;
 
 const AddWhisperBtn = ({ pageIndex, publication }) => {
   const { setPublication } = usePublicationContext();
   pageIndex = 2;
   const { onTabChange } = useBottomTab();
   return (
-    <ButtonBg
+    <div className={`w-[200px] h-[48px] rounded-[40px] mt-[50px] flex items-center justify-center z-10 cursor-pointer ${styles.Buttonbg}`}
       onClick={() => {
         setPublication(publication);
         onTabChange(TabItems[pageIndex]);
       }}
     >
-      <ButtonText> Add whisper to chain </ButtonText>
-    </ButtonBg>
+      <div className="not-italic -tracking-[0.03em] font-extrabold text-[16px] leading-[100%] text-center text-[#FFFFFF]"> Add whisper to chain </div>
+    </div>
   );
 };
 
