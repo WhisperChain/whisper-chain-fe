9157d452abcaaf7a2112f41dec69c875a9da2530
Gopal Jasani <100685534+GopalJasani777@users.noreply.github.com>
2022-12-21 17:19:52 +0530

    Share button functionality

--- src/components/ShareBtn.js
+++ src/components/ShareBtn.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useBottomTab } from "../context/BottomTabContext";
 import { usePublicationContext } from "../context/PublicationContext";
 import { TabItems } from "./Main/TabItems";
 import styles from "./AddWhisperBtn.module.css";
 
 const ShareBtn = ({ pageIndex, publication, height, width, text }) => {
-  const { setPublication } = usePublicationContext();
-  pageIndex = 1;
-  const { onTabChange } = useBottomTab();
+  const [ulrCopy, setUrlCopy] = useState(false);
+  const copyLink = () => {
+    const tempInut = document.createElement('input');
+    tempInut.value = window.location.href;
+    document.body.appendChild(tempInut);
+    tempInut.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInut); 
+    setUrlCopy(true);
+    setTimeout(() => {
+      setUrlCopy(false);
+    }, 3000);
+  };
   return (
     <a
       className={`w-[${width}px] h-[${height}px] rounded-[40px] flex items-center justify-center z-10 cursor-pointer ${styles.Buttonbg}`}
-      href="https://whisperchain.quick-poc.com/chain"
       target="_blank"
+      onClick={copyLink}
     >
       <div className="not-italic -tracking-[0.03em] font-extrabold text-[16px] leading-[100%] text-center text-[#FFFFFF]">
-        {text}
+      {!ulrCopy ? "Share" : "Url Copied!"}
       </div>
     </a>
   );
