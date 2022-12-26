4679048a32480569d3e86db839d72e842bfe7c25
Hardik Shah <hardikshah_2000@yahoo.com>
2022-12-26 15:57:07 +0530

    changed opacity for create acc btn

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -155,14 +155,13 @@ const Header = () => {
       console.log({ profileRes })
     }, 1000);
   }
-
+// var regex = /[`!@#$%^&*()&,.+\-=\[\]{};':"\\|<>\/?~]/;
   const handleCreateAcc = async () => {
     const res = await createProfile(userNameText);
     if (res) {
       if (res.data.createProfile?.reason === "HANDLE_TAKEN") {
         setUserNameTaken(true);
       }
-
       await pollIndexing(res.data.createProfile.txHash);
     }
   }
@@ -315,7 +314,9 @@ const Header = () => {
           <div>
             <a
               onClick={handleCreateAcc}
-              className={`mt-[15px] flex justify-center p-[10px] text-white cursor-pointer ${styles.createAccBtn}`}>
+              className={`mt-[15px] flex justify-center p-[10px] text-white cursor-pointer ${styles.createAccBtn} 
+                ${userNameEmpty || userNameTaken ? "opacity-50 cursor-not-allowed	pointer-events-none": ""}
+              `}>
               <span><CreateAccLogo /></span>
               <span className="ml-[10px]">Create account</span>
             </a>
