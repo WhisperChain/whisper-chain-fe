b61a40c84f04d70c8e6719d6122bc436b2e0a309
Gopal Jasani <100685534+GopalJasani777@users.noreply.github.com>
2022-12-26 17:18:46 +0530

    Dispatcher WIP

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,6 +10,7 @@ import styles from "./Header.module.css";
 import CustomConnectButton from "../ConnectButton";
 import CheckedCircle from "../../assets/CheckedCircle";
 import ProfileCreatedSucces from "../SignInModal/ProfileCreatedSucces";
+import SignTypedData from "../ConnectButton/SignTypedData";
 import CreateAccLogo from "../../assets/createAccLogo";
 import {
   createProfile,
@@ -368,14 +369,12 @@ const Header = () => {
             <div>
               <a
                 onClick={handleCreateAcc}
-                className={`mt-[15px] flex justify-center p-[10px] text-white cursor-pointer ${
-                  styles.createAccBtn
-                } 
-                ${
-                  userNameEmpty || userNameTaken
+                className={`mt-[15px] flex justify-center p-[10px] text-white cursor-pointer ${styles.createAccBtn
+                  } 
+                ${userNameEmpty || userNameTaken
                     ? "opacity-50 cursor-not-allowed	pointer-events-none"
                     : ""
-                }
+                  }
               `}
               >
                 <span>
@@ -393,55 +392,55 @@ const Header = () => {
       />
 
       {/* Enable dispatcher modal */}
-      <Modal
-        onRequestClose={signInModalCloseHandler}
-        isOpen={openSignInModal}
-        style={customModalStyles}
-        shouldCloseOnOverlayClick={false}
-        shouldCloseOnEsc={false}
-      >
-        <div
-          className={`flex flex-col justify-start items-start bg-[#FFFFFF] rounded-[16px] backdrop-blur-3xl gap-[16px] p-[40px] ${styles.ModalContainer}`}
-        >
-          <div>
-            <div
-              className={`flex justify-center box-border items-center w-[234px] h-[40px] bg-[#ABFE2C] text-[#00501E] backdrop-blur rounded-[4px] gap-[8px] cursor-pointer border-[1px] border-solid border-black/20`}
-            >
-              Enable Dispatcher
-            </div>
-          </div>
-          <div
-            className={`flex justify-start flex-col gap-[12px] not-italic text-[12px] font-medium ${styles.LensInfo}`}
+
+      {JSON.parse(window.localStorage.getItem("profile"))?.dispatcher
+        ?.address ? null : (
+        <div>
+          <Modal
+            onRequestClose={signInModalCloseHandler}
+            isOpen={openSignInModal}
+            style={customModalStyles}
+            shouldCloseOnOverlayClick={false}
+            shouldCloseOnEsc={false}
           >
-            <div className="flex gap-[8px] justify-start items-center">
-              <CheckedCircle />
-              Automatically sign transactions
+            <div
+              className={`flex flex-col justify-start items-start bg-[#FFFFFF] rounded-[16px] backdrop-blur-3xl gap-[16px] p-[40px] ${styles.ModalContainer}`}
+            >
+              <div>
+                <div
+                  className={`flex justify-center box-border items-center w-[234px] h-[40px] bg-[#ABFE2C] text-[#00501E] backdrop-blur rounded-[4px] gap-[8px] cursor-pointer border-[1px] border-solid border-black/20`}
+                  onClick={enableDispatcher}
+                >
+                  Enable Dispatcher
+                </div>
+              </div>
+              <div
+                className={`flex justify-start flex-col gap-[12px] not-italic text-[12px] font-medium ${styles.LensInfo}`}
+              >
+                <div className="flex gap-[8px] justify-start items-center">
+                  <CheckedCircle />
+                  Automatically sign transactions
+                </div>
+                <div className="flex gap-[8px] justify-start items-center">
+                  <CheckedCircle />
+                  Completely secure
+                </div>
+              </div>
             </div>
-            <div className="flex gap-[8px] justify-start items-center">
-              <CheckedCircle />
-              Completely secure
-            </div>
-          </div>
+          </Modal>
+          {Object.keys(typedData)?.length > 0 ? (
+            <SignTypedData
+              typedData={typedDataRef.current}
+              id={enableDispatcherTxnId.current}
+              onSuccess={async () => {
+                const profileRes = await getProfile(address);
+                const profile = profileRes.data.profiles.items[0];
+                window.localStorage.setItem("profile", JSON.stringify(profile));
+              }}
+            />
+          ) : null}
         </div>
-      </Modal>
-      {/* {JSON.parse(window.localStorage.getItem("profile"))?.dispatcher
-    //   ?.address ? null : (
-    //   <div>
-    //     <button style={{ color: "white" }} onClick={enableDispatcher}>
-    //       Enable Dispatcher
-    //     </button>
-    //     {Object.keys(typedData)?.length > 0 ? (
-    //       <SignTypedData
-    //         typedData={typedDataRef.current}
-    //         id={enableDispatcherTxnId.current}
-    //         onSuccess= {async ()=>{
-    //           const profileRes = await getProfile(address);
-    //           const profile = profileRes.data.profiles.items[0];
-    //           window.localStorage.setItem("profile", JSON.stringify(profile));}}
-    //       />
-    //     ) : null}
-    //   </div>
-    // )} */}
+      )}
     </>
   );
 };
