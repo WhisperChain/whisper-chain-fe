3dc9d330adc69b1970e02a4d929ee85edbff7afb
Hardik Shah <hardikshah_2000@yahoo.com>
2022-12-26 16:16:16 +0530

    created enable dispatcher modal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,10 +11,15 @@ import CustomConnectButton from "../ConnectButton";
 import CheckedCircle from "../../assets/CheckedCircle";
 import ProfileCreatedSucces from "../SignInModal/ProfileCreatedSucces";
 import CreateAccLogo from "../../assets/createAccLogo";
-import { createProfile, getProfile, refetchActiveQueries, txIndexed } from "../../utils/lensFunction";
+import {
+  createProfile,
+  getProfile,
+  refetchActiveQueries,
+  txIndexed,
+} from "../../utils/lensFunction";
 import { useAccount } from "wagmi";
 
-Modal.setAppElement('#header');
+Modal.setAppElement("#header");
 
 const customStyles = {
   content: {
@@ -29,7 +34,8 @@ const customStyles = {
     padding: "0px",
     top: "88px",
     border: "1px solid rgba(0, 0, 0, 0.2)",
-    boxShadow: "0px 8px 8px -8px rgba(0, 0, 0, 0.2), 0px 16px 24px rgba(0, 0, 0, 0.16)",
+    boxShadow:
+      "0px 8px 8px -8px rgba(0, 0, 0, 0.2), 0px 16px 24px rgba(0, 0, 0, 0.16)",
     cursor: "pointer",
   },
   overlay: {
@@ -52,7 +58,6 @@ const customModalStyles = {
   },
 };
 
-
 const Header = () => {
   const { address } = useAccount();
   const dispatcher = React.useRef(null);
@@ -69,17 +74,17 @@ const Header = () => {
   });
 
   const logoutModalCloseHandler = React.useCallback(() => {
-    setOpenSignOutModal(false)
-  })
+    setOpenSignOutModal(false);
+  });
 
   const logoutModalOpenHandler = React.useCallback(() => {
     setOpenSignOutModal(true);
-  })
+  });
 
   const [userNameTaken, setUserNameTaken] = React.useState(false);
   const [userNameEmpty, setUserNameEmpty] = React.useState(false);
   const [errorText, setErrorText] = React.useState("");
-  const [userNameText, setUserNameText] = React.useState('');
+  const [userNameText, setUserNameText] = React.useState("");
 
   const [createProfileModal, setCreateProfileModal] = React.useState(false);
 
@@ -98,17 +103,16 @@ const Header = () => {
   });
 
   const onInputChangeHandler = (e) => {
-    setUserNameText(e.target.value)
-    setUserNameEmpty(false)
-    setUserNameTaken(false)
-    if (!e.target.value.replace(/\s/g, '').length) {
-      setErrorText("Username can not be empty")
-      setUserNameEmpty(true)
+    setUserNameText(e.target.value);
+    setUserNameEmpty(false);
+    setUserNameTaken(false);
+    if (!e.target.value.replace(/\s/g, "").length) {
+      setErrorText("Username can not be empty");
+      setUserNameEmpty(true);
+    } else {
+      setUserNameEmpty(false);
     }
-    else {
-      setUserNameEmpty(false)
-    }
-  }
+  };
 
   let timeout = 0;
 
@@ -130,7 +134,7 @@ const Header = () => {
       if (isIndexedFlag) {
         // debugger;
         setTimeout(async () => {
-          await refetchActiveQueries()
+          await refetchActiveQueries();
           const profileRes = await getProfile(address);
           // debugger;
           const profile = profileRes.data.profiles.items[0];
@@ -146,15 +150,15 @@ const Header = () => {
           setTimeout(() => {
             successModalCloseHandler(true);
           }, 3000);
-        }, 1000)
+        }, 1000);
       }
     }, 5000);
 
     setTimeout(async () => {
       const profileRes = await getProfile(address);
-      console.log({ profileRes })
+      console.log({ profileRes });
     }, 1000);
-  }
+  };
 
   const handleCreateAcc = async () => {
     const res = await createProfile(userNameText);
@@ -165,170 +169,244 @@ const Header = () => {
 
       await pollIndexing(res.data.createProfile.txHash);
     }
-  }
+  };
 
-  return (<>
-    <div
-      className="px-[50px] flex-row flex justify-between items-center w-full z-[10000] relative pt-[40px]"
-      id="header"
-    >
-      <Logo />
-      <div className="flex ml-auto gap-[16px]">
-        <ConnectButton.Custom>
-          {({
-            account,
-            chain,
-            authenticationStatus,
-            mounted,
-          }) => {
-            // Note: If your app doesn't use authentication, you
-            // can remove all 'authenticationStatus' checks
-            const ready = mounted && authenticationStatus !== "loading";
-            const connected =
-              ready &&
-              account &&
-              chain &&
-              (!authenticationStatus || authenticationStatus === "authenticated");
-            return (
-              <div
-                {...(!ready && {
-                  "aria-hidden": true,
-                  style: {
-                    opacity: 0,
-                    pointerEvents: "none",
-                    userSelect: "none",
-                  },
-                })}
-              >
-                {(() => {
-                  if (!connected) {
-                    return (
-                      <button onClick={signInModalOpenHandler} className={` w-[92px] h-[36px] bg-[#FFFFFF] rounded-[40px] not-italic font-bold text-[#6F1AFF] ${styles.HeaderSignInbtn}`}>
-                        Sign In
-                      </button>
-                    );
-                  }
-
-                  return window?.localStorage.getItem("profileId") ? (
-                    <>
-                      <div className="flex justify-center items-center gap-[8px] z-[111] cursor-pointer" onClick={logoutModalOpenHandler}>
-                        <div className={`box-border flex justify-center items-center  py-[4px] px-[16px] w-auto h-[36px] bg-[#FFFFFF] rounded-[40px] not-italic font-bold text-[#6F1AFF] ${styles.HeaderSignInbtn}`}>
-                          {JSON.parse(window.localStorage.getItem("profile"))?.handle}
-                        </div>
-                        <Image
-                          src={getProfileImage() ?? "https://cdn.stamp.fyi/avatar/eth:0x3a72452af2ddc056330bbcb43898134c9adb51cf?s=250"}
-                          alt="profile"
-                          className="rounded-[18px]"
-                          width={36}
-                          height={36}
-                        />
-                      </div>
-                      <Modal onRequestClose={logoutModalCloseHandler} isOpen={openSignOutModal} style={customStyles} ariaHideApp={false}>
-                        <div className="flex flex-row items-center py-[15px] pl-[20px] color-red" onClick={
-                          () => {
-                            Router.reload();
-                            logoutModalCloseHandler();
-                            resetLocalStorage();
-                          }
-                        }
-                        >
-                          <SignOutLogo />
-                          <div className={`pl-[10px] ${styles.LogOutText}`}>Logout</div>
-                        </div>
-                      </Modal>
-                    </>
-                  ) :
-                    <></>
-                  // <CreateAccount
-                  //   onRequestClose={logoutModalCloseHandler}
-                  //   isOpen={open}
-                  // />
-                })()}
-              </div>
-            );
-          }}
-        </ConnectButton.Custom>
-      </div>
-    </div>
-
-    {/* Sign in modal */}
-    <Modal
-      onRequestClose={signInModalCloseHandler}
-      isOpen={openSignInModal}
-      style={customModalStyles}
-      shouldCloseOnOverlayClick={false}
-      shouldCloseOnEsc={false}
-    >
+  return (
+    <>
       <div
-        className={`flex flex-col justify-start items-start bg-[#FFFFFF] rounded-[16px] backdrop-blur-3xl gap-[16px] p-[40px] ${styles.ModalContainer}`}
+        className="px-[50px] flex-row flex justify-between items-center w-full z-[10000] relative pt-[40px]"
+        id="header"
       >
-        <CustomConnectButton onSignInComplete={signInModalCloseHandler} setCreateProfileModal={setCreateProfileModal} />
-        <div
-          className={`flex justify-start flex-col gap-[12px] not-italic text-[12px] font-medium ${styles.LensInfo}`}
-        >
-          <div className="flex gap-[8px] justify-start items-center">
-            <CheckedCircle />
-            Play with Lens frens
-          </div>
-          <div className="flex gap-[8px] justify-start items-center">
-            <CheckedCircle />
-            Post your whispers
-          </div>
-          <div className="flex gap-[8px] justify-start items-center">
-            <CheckedCircle />
-            Like and collect other whispers
-          </div>
-          <div className="flex justify-start items-center text-[13px] leading-[160%] font-medium text-[#00000099]">
-            New to Lens?
-            <CustomConnectButton onSignInComplete={signInModalCloseHandler} setCreateProfileModal={setCreateProfileModal} btnText={"Create an account"} />
-          </div>
-        </div>
-      </div>
-    </Modal>
+        <Logo />
+        <div className="flex ml-auto gap-[16px]">
+          <ConnectButton.Custom>
+            {({ account, chain, authenticationStatus, mounted }) => {
+              // Note: If your app doesn't use authentication, you
+              // can remove all 'authenticationStatus' checks
+              const ready = mounted && authenticationStatus !== "loading";
+              const connected =
+                ready &&
+                account &&
+                chain &&
+                (!authenticationStatus ||
+                  authenticationStatus === "authenticated");
+              return (
+                <div
+                  {...(!ready && {
+                    "aria-hidden": true,
+                    style: {
+                      opacity: 0,
+                      pointerEvents: "none",
+                      userSelect: "none",
+                    },
+                  })}
+                >
+                  {(() => {
+                    if (!connected) {
+                      return (
+                        <button
+                          onClick={signInModalOpenHandler}
+                          className={` w-[92px] h-[36px] bg-[#FFFFFF] rounded-[40px] not-italic font-bold text-[#6F1AFF] ${styles.HeaderSignInbtn}`}
+                        >
+                          Sign In
+                        </button>
+                      );
+                    }
 
-    {/* Create account on lens modal */}
-    <Modal
-      onRequestClose={createProfileModalCloseHandler}
-      isOpen={createProfileModal}
-      style={customModalStyles}
-      shouldCloseOnOverlayClick={false}
-      shouldCloseOnEsc={false}
-    >
-      <div className="flex justify-center items-center p-[30px]">
-        <div>
-          <h1 className="text-[16px] font-bold leading-[22px] text-[#000000] pb-[20px]">Create your lenster account</h1>
-          <input
-            type="text"
-            placeholder="Enter your desired username"
-            className={`p-[10px] ${styles.createInput}`}
-            onChange={(e) => onInputChangeHandler(e)}
-            value={userNameText}
-          />
-          {userNameTaken &&
-            <p className={`p-[10px] ${styles.errorMessage}`}>This username is already taken.
-              Please try <br /> again with a different one.
-            </p>
-          }
-          {userNameEmpty &&
-            <p className={`p-[5px] text-[#cf3838] ${styles.emptyText}`}>{errorText}</p>
-          }
-          <div>
-            <a
-              onClick={handleCreateAcc}
-              className={`mt-[15px] flex justify-center p-[10px] text-white cursor-pointer ${styles.createAccBtn} 
-                ${userNameEmpty || userNameTaken ? "opacity-50 cursor-not-allowed	pointer-events-none" : ""}
-              `}>
-              <span><CreateAccLogo /></span>
-              <span className="ml-[10px]">Create account</span>
-            </a>
-          </div>
+                    return window?.localStorage.getItem("profileId") ? (
+                      <>
+                        <div
+                          className="flex justify-center items-center gap-[8px] z-[111] cursor-pointer"
+                          onClick={logoutModalOpenHandler}
+                        >
+                          <div
+                            className={`box-border flex justify-center items-center  py-[4px] px-[16px] w-auto h-[36px] bg-[#FFFFFF] rounded-[40px] not-italic font-bold text-[#6F1AFF] ${styles.HeaderSignInbtn}`}
+                          >
+                            {
+                              JSON.parse(window.localStorage.getItem("profile"))
+                                ?.handle
+                            }
+                          </div>
+                          <Image
+                            src={
+                              getProfileImage() ??
+                              "https://cdn.stamp.fyi/avatar/eth:0x3a72452af2ddc056330bbcb43898134c9adb51cf?s=250"
+                            }
+                            alt="profile"
+                            className="rounded-[18px]"
+                            width={36}
+                            height={36}
+                          />
+                        </div>
+                        <Modal
+                          onRequestClose={logoutModalCloseHandler}
+                          isOpen={openSignOutModal}
+                          style={customStyles}
+                          ariaHideApp={false}
+                        >
+                          <div
+                            className="flex flex-row items-center py-[15px] pl-[20px] color-red"
+                            onClick={() => {
+                              Router.reload();
+                              logoutModalCloseHandler();
+                              resetLocalStorage();
+                            }}
+                          >
+                            <SignOutLogo />
+                            <div className={`pl-[10px] ${styles.LogOutText}`}>
+                              Logout
+                            </div>
+                          </div>
+                        </Modal>
+                      </>
+                    ) : (
+                      <></>
+                    );
+                    // <CreateAccount
+                    //   onRequestClose={logoutModalCloseHandler}
+                    //   isOpen={open}
+                    // />
+                  })()}
+                </div>
+              );
+            }}
+          </ConnectButton.Custom>
         </div>
       </div>
-    </Modal>
-    <ProfileCreatedSucces
-      onRequestClose={successModalCloseHandler}
-      isOpen={successModal} />
-  </>
+
+      {/* Sign in modal */}
+      <Modal
+        onRequestClose={signInModalCloseHandler}
+        isOpen={openSignInModal}
+        style={customModalStyles}
+        shouldCloseOnOverlayClick={false}
+        shouldCloseOnEsc={false}
+      >
+        <div
+          className={`flex flex-col justify-start items-start bg-[#FFFFFF] rounded-[16px] backdrop-blur-3xl gap-[16px] p-[40px] ${styles.ModalContainer}`}
+        >
+          <CustomConnectButton
+            onSignInComplete={signInModalCloseHandler}
+            setCreateProfileModal={setCreateProfileModal}
+          />
+          <div
+            className={`flex justify-start flex-col gap-[12px] not-italic text-[12px] font-medium ${styles.LensInfo}`}
+          >
+            <div className="flex gap-[8px] justify-start items-center">
+              <CheckedCircle />
+              Play with Lens frens
+            </div>
+            <div className="flex gap-[8px] justify-start items-center">
+              <CheckedCircle />
+              Post your whispers
+            </div>
+            <div className="flex gap-[8px] justify-start items-center">
+              <CheckedCircle />
+              Like and collect other whispers
+            </div>
+            <div className="flex justify-start items-center text-[13px] leading-[160%] font-medium text-[#00000099]">
+              New to Lens?
+              <CustomConnectButton
+                onSignInComplete={signInModalCloseHandler}
+                setCreateProfileModal={setCreateProfileModal}
+                btnText={"Create an account"}
+              />
+            </div>
+          </div>
+        </div>
+      </Modal>
+
+      {/* Create account on lens modal */}
+      <Modal
+        onRequestClose={createProfileModalCloseHandler}
+        isOpen={createProfileModal}
+        style={customModalStyles}
+        shouldCloseOnOverlayClick={false}
+        shouldCloseOnEsc={false}
+      >
+        <div className="flex justify-center items-center p-[30px]">
+          <div>
+            <h1 className="text-[16px] font-bold leading-[22px] text-[#000000] pb-[20px]">
+              Create your lenster account
+            </h1>
+            <input
+              type="text"
+              placeholder="Enter your desired username"
+              className={`p-[10px] ${styles.createInput}`}
+              onChange={(e) => onInputChangeHandler(e)}
+              value={userNameText}
+            />
+            {userNameTaken && (
+              <p className={`p-[10px] ${styles.errorMessage}`}>
+                This username is already taken. Please try <br /> again with a
+                different one.
+              </p>
+            )}
+            {userNameEmpty && (
+              <p className={`p-[5px] text-[#cf3838] ${styles.emptyText}`}>
+                {errorText}
+              </p>
+            )}
+            <div>
+              <a
+                onClick={handleCreateAcc}
+                className={`mt-[15px] flex justify-center p-[10px] text-white cursor-pointer ${
+                  styles.createAccBtn
+                } 
+                ${
+                  userNameEmpty || userNameTaken
+                    ? "opacity-50 cursor-not-allowed	pointer-events-none"
+                    : ""
+                }
+              `}
+              >
+                <span>
+                  <CreateAccLogo />
+                </span>
+                <span className="ml-[10px]">Create account</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Modal>
+      <ProfileCreatedSucces
+        onRequestClose={successModalCloseHandler}
+        isOpen={successModal}
+      />
+
+      {/* Enable dispatcher modal */}
+      <Modal
+        onRequestClose={signInModalCloseHandler}
+        isOpen={openSignInModal}
+        style={customModalStyles}
+        shouldCloseOnOverlayClick={false}
+        shouldCloseOnEsc={false}
+      >
+        <div
+          className={`flex flex-col justify-start items-start bg-[#FFFFFF] rounded-[16px] backdrop-blur-3xl gap-[16px] p-[40px] ${styles.ModalContainer}`}
+        >
+          <div>
+            <div
+              className={`flex justify-center box-border items-center w-[234px] h-[40px] bg-[#ABFE2C] text-[#00501E] backdrop-blur rounded-[4px] gap-[8px] cursor-pointer border-[1px] border-solid border-black/20`}
+            >
+              Enable Dispatcher
+            </div>
+          </div>
+          <div
+            className={`flex justify-start flex-col gap-[12px] not-italic text-[12px] font-medium ${styles.LensInfo}`}
+          >
+            <div className="flex gap-[8px] justify-start items-center">
+              <CheckedCircle />
+              Automatically sign transactions
+            </div>
+            <div className="flex gap-[8px] justify-start items-center">
+              <CheckedCircle />
+              Completely secure
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
