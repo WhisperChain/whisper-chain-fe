4c9ef74cb9da98d86966912d5d9e963a75690664
Hardik Shah <hardikshah_2000@yahoo.com>
2023-01-09 18:33:38 +0530

    new eye icon for home and chain page

---
+++ src/assets/BlackEyeIcon.js
@@ -0,0 +1,30 @@
+const BlackEyeIcon = () => {
+  return (
+    <svg
+      width="21"
+      height="20"
+      viewBox="0 0 21 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.5 4.375C4.25 4.375 1.75 10 1.75 10C1.75 10 4.25 15.625 10.5 15.625C16.75 15.625 19.25 10 19.25 10C19.25 10 16.75 4.375 10.5 4.375Z"
+        stroke="black"
+        stroke-opacity="0.8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.5 13.125C12.2259 13.125 13.625 11.7259 13.625 10C13.625 8.27411 12.2259 6.875 10.5 6.875C8.77411 6.875 7.375 8.27411 7.375 10C7.375 11.7259 8.77411 13.125 10.5 13.125Z"
+        stroke="black"
+        stroke-opacity="0.8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default BlackEyeIcon;
